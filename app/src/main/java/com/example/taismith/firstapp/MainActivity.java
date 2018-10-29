0c64a292dd1da3b3dd5d339da229ead9967328dd
taismith <tasmith8@aggies.ncat.edu>
2018-10-29 00:54:19 -0400

    All of the required tasks are complete.

--- app/src/main/java/com/example/taismith/firstapp/MainActivity.java
+++ app/src/main/java/com/example/taismith/firstapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.taismith.firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +17,19 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.flashcard_question).setBackgroundColor(getResources().getColor(R.color.my_red_color, null));
         findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.colorPrimary, null));
+
+        Button addButton;
+        addButton= findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
 
         final TextView questionTxt = findViewById(R.id.flashcard_question);
         final TextView answerTxt = findViewById(R.id.flashcard_answer);
@@ -30,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
                                     public void onClick(View v) {
                                         answerTxt.setVisibility(View.INVISIBLE);
                                         questionTxt.setVisibility(View.VISIBLE);
+
+
                                     }
 
 
@@ -41,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
                 });
 
 
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+            String string2 = data.getExtras().getString("string2");
+            final TextView questionTxt = findViewById(R.id.flashcard_question);
+            final TextView answerTxt = findViewById(R.id.flashcard_answer);
+            questionTxt.setText(string1);
+            answerTxt.setText(string2);
+        }
     }
 }
 
